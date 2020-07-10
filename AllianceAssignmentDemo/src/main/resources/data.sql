867b04a63938bf6daa28d946bd639a3265714e8e
korakot lee <lkorakot@gmail.com>
2020-07-10 17:50:45 +0700

    Initial project with basic REST API and H2 inmemory
    Signed-off-by: korakot lee <lkorakot@gmail.com>

---
+++ AllianceAssignmentDemo/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO employees (first_name, last_name, email_address) VALUES 
+('Douglas','MacArthur','DMacArthur@email.com'),
+('Dwight','Eisenhower','DEisenhower@email.com'),
+('George','Patton','GPatton@email.com'),
+('Omar','Bradley','OBradley@email.com'),
+('Henry','Arnold','HArnold@email.com'),
+('Jacob','Devers','JDevers@email.com'),
+('Harry','Truman','HTruman@email.com'),
+('Gerald','Ford','GFord@email.com'),
+('Chester','Nimitz','CNimitz@email.com');
